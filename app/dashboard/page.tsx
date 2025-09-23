1dd0119ce257e3f556801481c7c177985392f12d
elhamitiayoub <elhamitiayoub5@gmail.com>
2025-09-23 13:52:46 +0100

    Added protected dashboard route with Kinde authentication

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { redirect } from "next/navigation";
+
+export default async function DashboardRoute() {
+    const {getUser} = getKindeServerSession();
+    const user = await getUser();
+
+    if(!user) {
+        return redirect("/api/auth/register");
+    }
+
+    return (
+        <div>
+            <h1>Hello From The Dashboard</h1>
+        </div>
+    )
+}
